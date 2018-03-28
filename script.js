9b60da788f26efaa21c2fa9bcf27a27e78e307cb
Jack Tyrer <jack_tyrer@hotmail.co.uk>
2018-03-28 20:44:16 +0100

    Added styling

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-$(document).ready(function(){
 
 var myLat;
 var myLon;
-
 // Get user's location
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
@@ -14,24 +12,21 @@ if (navigator.geolocation) {
   $('#location').html("Location not available.");
   };
 
-
 function getWeather(myLat, myLon){
   $.getJSON("https://fcc-weather-api.glitch.me/api/current?lon="+myLon+"&lat="+myLat +'', function(json){
-
    $('#temp').html(JSON.stringify(Math.floor(json.main.temp)+"&#8451;").replace(/\"/g, ""));
    $('#location').html(JSON.stringify(json.name + ', ' + json.sys.country).replace(/\"/g, ""));
-
    $('#weatherIcon').html('<img class="icon" src="' + json.weather[0].icon + '">');
 }) //End of JSON
+
   }; //End of getWeather
 
-//Fahrenheit Celsius conversion
 
-$('#tempBtn').on('click', function(){
-  if(this.innerHTML === 'Fahrenheit'){
-    this.innerHTML = 'Celsius';
-  } else {
-    this.innerHTML = 'Fahrenheit';
-  }
-}); // End of conversion button
-   });//End of get function
+  $('#tempBtn').on('click', function(){
+    if(this.innerHTML === 'Switch to Fahrenheit'){
+      this.innerHTML = 'Switch to Celsius';
+    } else {
+      this.innerHTML = 'Switch to Fahrenheit';
+      temp = "test";
+    }
+  }); // End of conversion button
