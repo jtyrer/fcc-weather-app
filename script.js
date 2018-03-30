15d97c5353ef15810d3e5260498ffec8c0699735
Jack Tyrer <jack_tyrer@hotmail.co.uk>
2018-03-30 20:31:19 +0100

    Added min max temperature

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-//Not been able to get the Fahrenheit/Celsius button working
-
-// var fahrenheit = false;
-
 $(document).ready(function() {
-  //    $('#tempBtn').on('click', function(data){
-  //    if(this.innerHTML === 'Fahrenheit'){
-  //      this.innerHTML = 'Celsius';
-  //      fahrenheit = true;
-  //     update(data);
-  //    } else {
-  //      this.innerHTML = 'Fahrenheit';
-  //      fahrenheit = false;
-  //      update(data);
-  //    }
-  // });
 
   navigator.geolocation.getCurrentPosition(success, error);
 
@@ -38,7 +23,6 @@ $(document).ready(function() {
     var city = data.name;
     var country = data.sys.country;
     var temp = Math.round(data.main.temp);
-    //  var fahTemp = Math.round(data.main.temp) * 9/5 + 32;
     var icon = data.weather[0].icon;
     var tempMax = data.main.temp_max;
     var tempMin = data.main.temp_min;
@@ -46,11 +30,7 @@ $(document).ready(function() {
 
     $("#city").html(city);
     $("#country").html(country);
-    //    if (fahrenheit = true){
-    //      $('#temp').html(fahTemp);
-    //    } else {
     $("#temp").html(temp);
-    //    }
     $("#weatherIcon").attr("src", icon);
     $("#tempMax").html(tempMax);
     $("#tempMin").html(tempMin);
